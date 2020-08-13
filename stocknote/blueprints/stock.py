560068016415ccbe93f7b59b008f670d30107738
lhq <20097012@cqu.edu.cn>
2020-08-13 22:14:27 +0800

    fix: 优化个股的自由现金流和财务健康的表格

--- stocknote/blueprints/stock.py
+++ stocknote/blueprints/stock.py
@@ -63,14 +63,19 @@ def edit_basic_info(code):
 
 @stock_bp.route("/<code>/free-cashflow", methods=["GET"])
 def free_cashflow(code):
-    cfr_ratios = get_cashflow_revenue_ratios(code)
-    q = StockCashFlow.query.filter_by(code=code).order_by(StockCashFlow.account_date).all()
+    q = db.session.query(StockCashFlow.net_operating_cashflow, StockCashFlow.account_date, StockIncomeStatement.revenue, StockIncomeStatement.net_profit) \
+        .filter(StockCashFlow.code==code, StockCashFlow.code==StockIncomeStatement.code)  \
+        .filter(StockCashFlow.account_date==StockIncomeStatement.account_date)  \
+        .order_by(StockCashFlow.account_date)  \
+        .all()
+
     data = []
     pre_cf = None
     for item in q:
         new_item = dict()
-        new_item["cfr_ratio"] = cfr_ratios.get(item.account_date)
         new_item["account_date"] = item.account_date
+        new_item["cfr_ratio"] = 100 * item.net_operating_cashflow / item.revenue
+        new_item["cfnp_ratio"] = 100 * item.net_operating_cashflow / item.net_profit
         new_item["net_operating_cashflow"] = item.net_operating_cashflow
         if pre_cf is None:
             new_item["nocf_yoy"] = None
@@ -79,7 +84,7 @@ def free_cashflow(code):
         pre_cf = item.net_operating_cashflow
         data.append(new_item)
     data.sort(key=itemgetter("account_date"), reverse=True)
-    return render_template("stock/_cashflow_table.html", cashflows=data)
+    return render_template("stock/parts/_cashflow_table.html", cashflows=data)
 
 
 @stock_bp.route("/<code>/income-percentage", methods=["GET"])
@@ -102,7 +107,7 @@ def income_percentage(code):
         new_item["op"] = 100 * item.op / revenue
         new_item["others"] = new_item["gross_rofit_margin"] - new_item["manage_fee"] -  new_item["rad_cost"] - new_item["sales_fee"] - new_item["financing_expenses"] - new_item["op"]
         data.append(new_item)
-    return render_template("stock/_income_percentage_table.html", data=data)
+    return render_template("stock/parts/_income_percentage_table.html", data=data)
 
 
 @stock_bp.route("/valuation", methods=["GET"])
@@ -111,7 +116,7 @@ def valuation():
     g = request.args.get("g", default=0.03, type=float)    #  永续年金增长率
     discount_rate = request.args.get("discountRate", default=0.09, type=float)   # 折现率
     total_equity = request.args.get("totalEquity", type=int)
-    initial_cashflow = request.args.get("initialStockCashFlow", type=float)
+    initial_cashflow = request.args.get("initialCashFlow", type=float)
     _cashflow_growths = request.args.get("cashFlowGrowths", type=str)
 
     if initial_cashflow is None or _cashflow_growths is None:
@@ -141,7 +146,7 @@ def valuation():
     # 永续年金折现价值
     data["present_value_of_perpetuity_value"] = int(data["perpetuity_value"] / pow(1 + discount_rate, len(cashflow_growths)))
     data["final_valuation"] = data["present_value_of_perpetuity_value"] + data["pv_total"]
-    return render_template("stock/_valuation_table.html", params=params, data=data)
+    return render_template("stock/parts/_valuation_table.html", params=params, data=data)
 
 
 @stock_bp.route("/<code>/financial-indicators", methods=["GET"])
@@ -159,7 +164,7 @@ def financial_indicators(code):
         new_item["number_of_times_interest_earned"] = "数据缺失"     # 已获利息倍数
         simple_indicators.append(new_item)
     simple_indicators.sort(key=itemgetter("account_date"), reverse=True)
-    return render_template("stock/_indicators_table.html", indicators=simple_indicators)
+    return render_template("stock/parts/_indicators_table.html", indicators=simple_indicators)
 
 
 @stock_bp.route("/<code>/data/revenue", methods=["GET"])
@@ -238,10 +243,9 @@ def api_data_net_profit(code):
 
 @stock_bp.route("/<code>/data/profitablity", methods=["GET"])
 def api_data_profitablity(code):
-    cfr_ratios = get_cashflow_revenue_ratios(code)
     indicators = get_stock_indicators(code)
     x_ticks = []
-    cashflow_revenue, mll, jll, roa, roe = [], [], [], [], []
+    mll, jll, roa, roe = [], [], [], []
     for item in indicators:
         account_date = item.account_date
         x_ticks.append(account_date.strftime("%Y年报"))
@@ -249,28 +253,14 @@ def api_data_profitablity(code):
         roa.append(item.net_interest_of_total_assets)
         mll.append(item.gross_selling_rate)
         jll.append(item.net_selling_rate)
-        cashflow_revenue.append(cfr_ratios.get(account_date))
 
     def map_round(data, n=2):
         return list(map(lambda x: round(x, n), data))
-
-    # data = {
-    #         "xLabels": x_labels,
-    #         "yName": "比率(%)",
-    #         "items": [
-    #             {"name": "自由现金流/营业收入", "values": map_round(cashflow_revenue)},
-    #             {"name": "销售毛利率", "values": map_round(mll)},
-    #             {"name": "销售净利率", "values": map_round(jll)},
-    #             {"name": "ROE", "values": map_round(roe)},
-    #             {"name": "ROA", "values": map_round(roa)},
-    #         ]
-    #     }
 
     data = {
         "y_label": "比率(%)",
         "x_ticks": x_ticks,
         "values": {
-            "自由现金流/营业收入": map_round(cashflow_revenue),
             "销售毛利率": map_round(mll),
             "销售净利率": map_round(jll),
             "ROE": map_round(roe),
