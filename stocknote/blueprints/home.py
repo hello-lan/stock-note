c876c7c8b5be14b434b5a7eb136f5a84d6265a1c
lhq <20097012@cqu.edu.cn>
2021-09-26 23:16:15 +0800

    feat: 添加执行个股财报数据爬虫的api

--- stocknote/blueprints/home.py
+++ stocknote/blueprints/home.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-from flask import render_template, current_app, Blueprint, jsonify
+from flask import render_template, current_app, Blueprint, jsonify, request
 
 
 home_bp = Blueprint("home", __name__)
@@ -8,3 +8,15 @@ home_bp = Blueprint("home", __name__)
 @home_bp.route("/")
 def index():
     return render_template("home/index.html")
+
+
+@home_bp.route("/shell/crawl")
+def api_op_shell_crawl_data():
+    code = request.args.get("code", type=str)
+    import subprocess
+    return_code = subprocess.call("cd scripts && bash load_stockdata.sh %s" % code, shell=True)
+    if return_code == 0:
+        msg = "succeed in crawling %s" % code
+    else:
+        msg = "failed to crawl %s" % code
+    return jsonify(status=200, returnCode=return_code, message=msg)
